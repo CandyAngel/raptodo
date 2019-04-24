08660b042cf2db096dd70dcfac8a5373b68d00ef
CandyAngel <candyangel@electricjungle.org>
2019-04-24 22:31:59 +0100

    Add documentation for todo.txt model

--- lib/RaptoDo/Model/TodoTxt.pm
+++ lib/RaptoDo/Model/TodoTxt.pm
@@ -21,3 +21,50 @@ sub items {
 }
 
 1;
+
+=encoding utf8
+
+=head1 NAME
+
+RaptoDo::Model::TodoTxt - Model for todo.txt files
+
+=head1 SYNOPSIS
+
+  use RaptoDo::Model::TodoTxt;
+
+  my $collection = RaptoDo::Model::TodoTxt->new(path => 'todo.txt')->items;
+
+=head1 DESCRIPTION
+
+L<RaptoDo::Model::TodoTxt> is a model for todo.txt files, which provides a
+simple API for reading.
+
+=head1 ATTRIBUTES
+
+L<RaptoDo::Model::TodoTxt> implements the following attributes.
+
+=head2 path
+
+  my $path = $model->path;
+  $model   = $model->path('/home/owen/todo.txt');
+
+Path to the todo.txt file to read, defaults to 'todo.txt'.
+
+=head1 METHODS
+
+L<RaptoDo::Model::TodoTxt> inherits all methods from L<Mojo::Base> and
+implements the following new ones.
+
+=head2 items
+
+  my $collection = $model->items;
+
+Reads the files and returns a L<Mojo::Collection> object containing the items
+as L<RaptoDo::Model::TodoTxt::Item> objects.
+
+=head1 SEE ALSO
+
+L<RaptoDo>, L<RaptoDo::Model::TodoTxt::Item>, L<Mojolicious>,
+L<https://mojolicious.org>.
+
+=cut
